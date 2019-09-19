442134547092bd29b3e4f65416498fa42d4c0c5e
cdiego55 <covielladiego@ufl.edu>
2019-09-18 20:46:51 -0400

    final

--- Bootcamp2/ListingSchema.js
+++ Bootcamp2/ListingSchema.js
@@ -1,14 +1,24 @@
 /* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
+    mongoose.set('useCreateIndex',true)
 
 /* Create your schema for the data in the listings.json file that will define how data is saved in your database
      See https://mongoosejs.com/docs/guide.html for examples for creating schemas
      See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
   */
 var listingSchema = new Schema({
+
   /* Your code for a schema here */ 
   //Check out - https://mongoosejs.com/docs/guide.html
+
+  code: {type: String, required: true, unique: true},
+  name: {type: String, required: true},
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String
 
 });
 
@@ -17,11 +27,22 @@ var listingSchema = new Schema({
 */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  // gets the date
+  var currentDate = new Date();
+
+  // updates
+  this.updated_at = currentDate;
+
+  // initial creation
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
 //Check out - https://mongoosejs.com/docs/guide.html#models
-var Listing = mongoose.model('Listing', listingSchema);
+var Listing = mongoose.model('listings', listingSchema);
 
 /* Export the model to make it avaiable to other parts of your Node application */
 module.exports = Listing;
